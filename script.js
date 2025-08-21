e109ccfd5fabab7d3ad811e875502a4e84dedb2d
GAGAN-ESWAR <gaganeswar.26@gmail.com>
2025-08-21 17:44:15 +0530

    add it to screen out of console
    each cell button is added and event listeners are
    added to play alternatively "x" and "o"

--- script.js
+++ script.js
@@ -121,8 +121,61 @@ function gameController(
 
     return {
         playRound,
-        getActivePlayer
+        getActivePlayer,
+        getBoard : board.getBoard
     };
 }
 
-const game = gameController();
+function screenController() {
+    const game = gameController();
+    const playerTurnDiv = document.querySelector(".turn");
+    const boardDiv = document.querySelector(".board");
+
+    const updateScreen = ()=>{
+        // clear the board
+        boardDiv.textContent = "";
+
+        // get the newest version of the board and player turn
+        const board = game.getBoard();
+        const activePlayer = game.getActivePlayer();
+
+        // Display player's turn
+        playerTurnDiv.textContent = `${activePlayer.name}'s turn...`
+
+        // Render board squares
+        board.forEach((row,indexRow) => {
+            row.forEach((cell,indexCol) => {
+                // Anything clickable should be a button!!
+                const cellButton = document.createElement("button");
+                cellButton.classList.add("cell");
+
+                // Create a data attribute to identify the column adn row
+                // This makes it easier to pass into our `playRound` function
+                cellButton.dataset.column = indexCol;
+                cellButton.dataset.row = indexRow;
+                cellButton.textContent = cell.getValue();
+                boardDiv.appendChild(cellButton);
+            })
+        })
+    };
+
+    // Add event listener for the board
+    function clickHandlerBoard (e) {
+        const selectedColumn = e.target.dataset.column;
+        const selectedRow = e.target.dataset.row;
+
+        if(!selectedColumn) return;
+
+        game.playRound(selectedRow,selectedColumn);
+        updateScreen();
+    }
+
+    boardDiv.addEventListener("click",clickHandlerBoard);
+
+    // Initial render
+    updateScreen();
+
+    // We don't need to return anything from this module because everything is encapsulated inside this screen controller.
+}
+
+screenController();
